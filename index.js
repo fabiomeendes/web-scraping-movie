a7073b402e5c8542bf9bcbc3511b5b714328bdce
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-05-07 11:09:54 -0300

    Fetch the page data

--- index.js
+++ index.js
@@ -4,7 +4,26 @@ const puppeteer = require('puppeteer');
   let movieUrl = 'https://www.imdb.com/title/tt0848228/';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(movieUrl);  
+  await page.goto(movieUrl, { waitUntil: 'networkidle2'});  
+
+  let data = await page.evaluate(() => {
+    let title = document.querySelector('div[class=title_wrapper] > h1').innerText;
+    let rating = document.querySelector('span[itemprop=ratingValue]').innerText;
+    let ratingCount = document.querySelector('span[itemprop=ratingCount]').innerText;
+    let duration = document.querySelector('time[datetime=PT143M]').innerText;
+
+    return {
+      title,
+      rating,
+      ratingCount,
+      duration
+    }
+  });
+
+  console.log(data, 'data');
+
+  // debugger;
 
   await browser.close();
+
 })();
